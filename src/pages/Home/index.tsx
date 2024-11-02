43b863351eb3dd108c45bd37cdef5384adbd3191
Bruno da Rosa Faller <fallerbruno@gmail.com>
2024-11-02 10:13:57 -0300

    fix: :sparkles: modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,173 +11,177 @@ import {
   SubTitle,
   TextContainer,
   Title,
-} from './styles'
-import banner from '../../assets/banner.png'
-import { Clock, Coffee, Package, ShoppingCart } from 'phosphor-react'
-import { CoffeeCard } from './components/CoffeeCard'
-import tradicional from '../../assets/Type=Expresso.png'
-import americano from '../../assets/Coffee.png'
-import cremoso from '../../assets/Type=Expresso Cremoso.png'
-import gelado from '../../assets/Type=Café Gelado.png'
-import comLeite from '../../assets/Type=Café com Leite.png'
-import latte from '../../assets/Type=Latte.png'
-import capuccino from '../../assets/Type=Capuccino.png'
-import macchiato from '../../assets/Type=Macchiato.png'
-import mocaccino from '../../assets/Type=Mochaccino.png'
-import chocoQuente from '../../assets/Type=Chocolate Quente.png'
-import cubano from '../../assets/Type=Cubano.png'
-import havaiano from '../../assets/Type=Havaiano.png'
-import arabe from '../../assets/Type=Árabe.png'
-import irlandes from '../../assets/Type=Irlandês.png'
+} from "./styles";
+import banner from "../../assets/banner.png";
+import { Clock, Coffee, Package, ShoppingCart } from "phosphor-react";
+import { CoffeeCard } from "./components/CoffeeCard";
+import tradicional from "../../assets/Type=Expresso.png";
+import americano from "../../assets/Coffee.png";
+import cremoso from "../../assets/Type=Expresso Cremoso.png";
+import gelado from "../../assets/Type=Café Gelado.png";
+import comLeite from "../../assets/Type=Café com Leite.png";
+import latte from "../../assets/Type=Latte.png";
+import capuccino from "../../assets/Type=Capuccino.png";
+import macchiato from "../../assets/Type=Macchiato.png";
+import mocaccino from "../../assets/Type=Mochaccino.png";
+import chocoQuente from "../../assets/Type=Chocolate Quente.png";
+import cubano from "../../assets/Type=Cubano.png";
+import havaiano from "../../assets/Type=Havaiano.png";
+import arabe from "../../assets/Type=Árabe.png";
+import irlandes from "../../assets/Type=Irlandês.png";
+import { Modal } from "../../components/Modal";
+import { useState } from "react";
+import { Cart } from "../Cart";
 
 export interface Tag {
-  id: number
-  name: string
+  id: number;
+  name: string;
 }
 
 export interface CoffeeType {
-  img: string
-  name: string
-  description: string
-  value: number
-  tag: Tag[]
+  img: string;
+  name: string;
+  description: string;
+  value: number;
+  tag: Tag[];
 }
 
 const coffes: CoffeeType[] = [
   {
     img: tradicional,
-    name: 'Expresso Tradicional',
-    description: 'O tradicional café feito com água quente e grãos moídos',
+    name: "Expresso Tradicional",
+    description: "O tradicional café feito com água quente e grãos moídos",
     value: 9.9,
-    tag: [{ id: 1, name: 'TRADICIONAL' }],
+    tag: [{ id: 1, name: "TRADICIONAL" }],
   },
   {
     img: americano,
-    name: 'Expresso Americano',
-    description: 'Expresso diluído, menos intenso que o tradicional',
+    name: "Expresso Americano",
+    description: "Expresso diluído, menos intenso que o tradicional",
     value: 10.9,
-    tag: [{ id: 2, name: 'TRADICIONAL' }],
+    tag: [{ id: 2, name: "TRADICIONAL" }],
   },
   {
     img: cremoso,
-    name: 'Expresso Cremoso',
-    description: 'Café expresso tradicional com espuma cremosa',
+    name: "Expresso Cremoso",
+    description: "Café expresso tradicional com espuma cremosa",
     value: 10.0,
-    tag: [{ id: 3, name: 'TRADICIONAL' }],
+    tag: [{ id: 3, name: "TRADICIONAL" }],
   },
   {
     img: gelado,
-    name: 'Expresso Gelado',
-    description: 'Bebida preparada com café expresso e cubos de gelo',
+    name: "Expresso Gelado",
+    description: "Bebida preparada com café expresso e cubos de gelo",
     value: 12.9,
     tag: [
-      { id: 4, name: 'TRADICIONAL' },
-      { id: 5, name: 'GELADO' },
+      { id: 4, name: "TRADICIONAL" },
+      { id: 5, name: "GELADO" },
     ],
   },
   {
     img: comLeite,
-    name: 'Café com Leite',
-    description: 'Meio a meio de expresso tradicional com leite vaporizado',
+    name: "Café com Leite",
+    description: "Meio a meio de expresso tradicional com leite vaporizado",
     value: 9.9,
     tag: [
-      { id: 6, name: 'TRADICIONAL' },
-      { id: 7, name: 'COM LEITE' },
+      { id: 6, name: "TRADICIONAL" },
+      { id: 7, name: "COM LEITE" },
     ],
   },
   {
     img: latte,
-    name: 'Latte',
+    name: "Latte",
     description:
-      'Uma dose de café expresso com o dobro de leite e espuma cremosa',
+      "Uma dose de café expresso com o dobro de leite e espuma cremosa",
     value: 10.9,
     tag: [
-      { id: 8, name: 'TRADICIONAL' },
-      { id: 9, name: 'COM LEITE' },
+      { id: 8, name: "TRADICIONAL" },
+      { id: 9, name: "COM LEITE" },
     ],
   },
   {
     img: capuccino,
-    name: 'Capuccino',
+    name: "Capuccino",
     description:
-      'Bebida com canela feita de doses iguais de café, leite e espuma',
+      "Bebida com canela feita de doses iguais de café, leite e espuma",
     value: 11.9,
     tag: [
-      { id: 10, name: 'TRADICIONAL' },
-      { id: 11, name: 'COM LEITE' },
+      { id: 10, name: "TRADICIONAL" },
+      { id: 11, name: "COM LEITE" },
     ],
   },
   {
     img: macchiato,
-    name: 'Macchiato',
+    name: "Macchiato",
     description:
-      'Café expresso misturado com um pouco de leite quente e espuma',
+      "Café expresso misturado com um pouco de leite quente e espuma",
     value: 12.9,
     tag: [
-      { id: 12, name: 'TRADICIONAL' },
-      { id: 13, name: 'COM LEITE' },
+      { id: 12, name: "TRADICIONAL" },
+      { id: 13, name: "COM LEITE" },
     ],
   },
   {
     img: mocaccino,
-    name: 'Mocaccino',
-    description: 'Café expresso com calda de chocolate, pouco leite e espuma',
+    name: "Mocaccino",
+    description: "Café expresso com calda de chocolate, pouco leite e espuma",
     value: 13.9,
     tag: [
-      { id: 14, name: 'TRADICIONAL' },
-      { id: 15, name: 'COM LEITE' },
+      { id: 14, name: "TRADICIONAL" },
+      { id: 15, name: "COM LEITE" },
     ],
   },
 
   {
     img: chocoQuente,
-    name: 'Chocolate Quente',
-    description: 'Bebida feita com chocolate dissolvido no leite quente e café',
+    name: "Chocolate Quente",
+    description: "Bebida feita com chocolate dissolvido no leite quente e café",
     value: 15.9,
     tag: [
-      { id: 18, name: 'ESPECIAL' },
-      { id: 19, name: 'COM LEITE' },
+      { id: 18, name: "ESPECIAL" },
+      { id: 19, name: "COM LEITE" },
     ],
   },
   {
     img: cubano,
-    name: 'Cubano',
+    name: "Cubano",
     description:
-      'Drink gelado de café expresso com rum, creme de leite e hortelã',
+      "Drink gelado de café expresso com rum, creme de leite e hortelã",
     value: 16.9,
     tag: [
-      { id: 20, name: 'ESPECIAL' },
-      { id: 21, name: 'ALCOÓLICO' },
-      { id: 22, name: 'GELADO' },
+      { id: 20, name: "ESPECIAL" },
+      { id: 21, name: "ALCOÓLICO" },
+      { id: 22, name: "GELADO" },
     ],
   },
   {
     img: havaiano,
-    name: 'Havaiano',
-    description: 'Bebida adocicada preparada com café e leite de coco',
+    name: "Havaiano",
+    description: "Bebida adocicada preparada com café e leite de coco",
     value: 16.9,
-    tag: [{ id: 23, name: 'ESPECIAL' }],
+    tag: [{ id: 23, name: "ESPECIAL" }],
   },
   {
     img: arabe,
-    name: 'Árabe',
-    description: 'Bebida preparada com grãos de café árabe e especiarias',
+    name: "Árabe",
+    description: "Bebida preparada com grãos de café árabe e especiarias",
     value: 16.9,
-    tag: [{ id: 24, name: 'ESPECIAL' }],
+    tag: [{ id: 24, name: "ESPECIAL" }],
   },
   {
     img: irlandes,
-    name: 'Irlandês',
-    description: 'Bebida preparada com grãos de café árabe e especiarias',
+    name: "Irlandês",
+    description: "Bebida preparada com grãos de café árabe e especiarias",
     value: 16.9,
     tag: [
-      { id: 25, name: 'ESPECIAL' },
-      { id: 26, name: 'ALCOÓLICO' },
+      { id: 25, name: "ESPECIAL" },
+      { id: 26, name: "ALCOÓLICO" },
     ],
   },
-]
+];
 
 export function Home() {
+  const [open, setOpen] = useState(false);
   return (
     <HomeContainer>
       <BannerContainer>
@@ -223,6 +227,9 @@ export function Home() {
       </BannerContainer>
       <MenuContainer>
         <MenuTitle>Nossos Cafés</MenuTitle>
+        <Modal isOpen={open} setIsOpen={setOpen}>
+          <Cart />
+        </Modal>
         <Menu>
           {coffes.map((item) => (
             <CoffeeCard
@@ -237,5 +244,5 @@ export function Home() {
         </Menu>
       </MenuContainer>
     </HomeContainer>
-  )
+  );
 }
